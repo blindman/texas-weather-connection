005625a362f646e138a070a5e52fa716f15f3813
blindman <jon.heller02@gmail.com>
2017-08-18 17:06:44 -0400

    create functionality to retrieve KBDI, TFD, and NEXRAD data

--- index.js
+++ index.js
@@ -1,2 +1,47 @@
 'use strict';
-module.exports = () => {};
+const http = require('http');
+const querystring = require('querystring');
+
+const ENDPOINT = 'http://twc.tamu.edu';
+
+class Resource {
+	constructor(data) {
+		this.name = data.Name;
+		this.prefix = data.Prefix;
+		this.fileType = data.FileType;
+		this.date = new Date(parseInt(data.Date.substring(6, data.Date.length - 2), 10));
+		this.url = ENDPOINT + data.Url;
+	}
+}
+
+const get = (date, type, successFn) => {
+	if (!date) {
+		throw new Error('No date was provided.');
+	}
+
+	if (!successFn) {
+		throw new Error('No callback was provided to handle the data (this operation is synchronous only in its current version).');
+	}
+
+	const qs = querystring.stringify({type, date});
+	http.get(ENDPOINT + '/getMaps?' + qs, response => {
+		let result = '';
+		response.setEncoding('utf8');
+
+		response.on('end', () => {
+			successFn(JSON.parse(result).map(val => new Resource(val)));
+		});
+
+		response.on('data', chunk => {
+			result += chunk;
+		});
+	}).on('error', error => {
+		throw new Error(error);
+	});
+};
+
+module.exports = {
+	getKBDI: (date, successFn) => get(date, 'KBDI', successFn),
+	getTFD: (date, successFn) => get(date, 'TFD', successFn),
+	getNEXRAD: (date, successFn) => get(date, 'NEXRAD', successFn)
+};
--- test.js
+++ test.js
@@ -1,4 +1,68 @@
 import test from 'ava';
 import fn from '.';
 
-test('title', t => {});
+test.cb('KBDI', t => {
+	t.plan(11);
+
+	const missingDate = t.throws(() => fn.getKBDI(), Error);
+	t.is(missingDate.message, 'No date was provided.');
+
+	const missingCb = t.throws(() => fn.getKBDI('8/17/17'), Error);
+	t.is(missingCb.message, 'No callback was provided to handle the data (this operation is synchronous only in its current version).');
+
+	fn.getKBDI('8/14/17', data => {
+		t.is(Object.prototype.toString.call(data), '[object Array]');
+		t.is(Object.prototype.toString.call(data[0].name), '[object String]');
+		t.is(Object.prototype.toString.call(data[0].prefix), '[object String]');
+		t.is(Object.prototype.toString.call(data[0].prefix), '[object String]');
+		t.is(Object.prototype.toString.call(data[0].fileType), '[object String]');
+		t.is(Object.prototype.toString.call(data[0].date), '[object Date]');
+		t.is(Object.prototype.toString.call(data[0].url), '[object String]');
+
+		t.end();
+	});
+});
+
+test.cb('TFD', t => {
+	t.plan(11);
+
+	const missingDate = t.throws(() => fn.getTFD(), Error);
+	t.is(missingDate.message, 'No date was provided.');
+
+	const missingCb = t.throws(() => fn.getTFD('8/17/17'), Error);
+	t.is(missingCb.message, 'No callback was provided to handle the data (this operation is synchronous only in its current version).');
+
+	fn.getTFD('8/14/17', data => {
+		t.is(Object.prototype.toString.call(data), '[object Array]');
+		t.is(Object.prototype.toString.call(data[0].name), '[object String]');
+		t.is(Object.prototype.toString.call(data[0].prefix), '[object String]');
+		t.is(Object.prototype.toString.call(data[0].prefix), '[object String]');
+		t.is(Object.prototype.toString.call(data[0].fileType), '[object String]');
+		t.is(Object.prototype.toString.call(data[0].date), '[object Date]');
+		t.is(Object.prototype.toString.call(data[0].url), '[object String]');
+
+		t.end();
+	});
+});
+
+test.cb('NEXRAD', t => {
+	t.plan(11);
+
+	const missingDate = t.throws(() => fn.getNEXRAD(), Error);
+	t.is(missingDate.message, 'No date was provided.');
+
+	const missingCb = t.throws(() => fn.getNEXRAD('8/17/17'), Error);
+	t.is(missingCb.message, 'No callback was provided to handle the data (this operation is synchronous only in its current version).');
+
+	fn.getNEXRAD('8/14/17', data => {
+		t.is(Object.prototype.toString.call(data), '[object Array]');
+		t.is(Object.prototype.toString.call(data[0].name), '[object String]');
+		t.is(Object.prototype.toString.call(data[0].prefix), '[object String]');
+		t.is(Object.prototype.toString.call(data[0].prefix), '[object String]');
+		t.is(Object.prototype.toString.call(data[0].fileType), '[object String]');
+		t.is(Object.prototype.toString.call(data[0].date), '[object Date]');
+		t.is(Object.prototype.toString.call(data[0].url), '[object String]');
+
+		t.end();
+	});
+});
